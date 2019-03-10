6c42d52795caa0dd1a7e6cfc8a87bea38a85b0d4
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-10 17:10:54 +0900

    [fix]

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
     def gravator_for(user)
         gravator_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravator_url = "https://secure.gravatar.com/avatar/#{gravaor_id}"
+        gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
 end
