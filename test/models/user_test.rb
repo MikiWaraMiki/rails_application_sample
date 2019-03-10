6323eb9ac90a5f988b333fa57419f61053f57f5b
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-10 12:47:32 +0900

    [fix]メールアドレス一意テストコードのバグフィックス
    user_testのメールアドレス一意テストコードのバグを修正しました

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,7 +38,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email should be unique" do
-    duplicate_user = user.dup
+    duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
