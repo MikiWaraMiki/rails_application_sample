a2c15df2958cdfe41fcf4f73347520746ef0f721
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-11 21:39:00 +0900

    [update]sign-upページのテストを実装

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  get signup_path
+  assert_no_difference 'User.count' do
+    post users_path, params:{
+      user:{
+        name:                  "",
+        email:                 "user@invalid.com",
+        password:              "sample",
+        password_confirmation: "sample_sample"
+      }
+    }
+  end
+  assert_template 'users/new'
 end
