13205a3ed80c7e7672684b0674bb67f77f049d69
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-11 23:39:21 +0900

    [add]ログイン検証をするテストコードを実装しました

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "login with invalid infromation" do
+    #loginページへ遷移
+    get login_path
+    #テンプレートが正しいことを検証
+    assert_template "sessions/new"
+    post login_path, params{
+      session:{
+        email:"sampel.com",
+        password:""
+      }
+    }
+    assert_template "sessions/new"
+    assert_not flash.empty?
+    get root_path
+    asser flash.empty?
+  end
 end
