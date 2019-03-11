8928424343ba4e6f3a408853c0853d0c8b8c02be
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-11 21:43:01 +0900

    [fix]テストコードのバグフィックス

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,16 +1,18 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get signup_path
-  assert_no_difference 'User.count' do
-    post users_path, params:{
-      user:{
-        name:                  "",
-        email:                 "user@invalid.com",
-        password:              "sample",
-        password_confirmation: "sample_sample"
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params:{
+        user:{
+          name:                  "",
+          email:                 "user@invalid.com",
+          password:              "sample",
+          password_confirmation: "sample_sample"
+        }
       }
-    }
+    end
+    assert_template 'users/new'
   end
-  assert_template 'users/new'
 end
