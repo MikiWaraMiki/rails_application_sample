2124607866283f3b06b55f6dad7feeeca5aec4da
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-11 22:06:48 +0900

    [fix]テストコードの修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,7 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
-    assert_select "div#<CSS id for error explanation>"
-    assert_select "div.<CSS class for field with error>"
+    assert_select "div#error_explanation"
+    assert_select "div.alert"
   end
 end
