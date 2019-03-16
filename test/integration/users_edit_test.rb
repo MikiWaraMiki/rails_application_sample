58bed9202a541d00ec17c09b44d6ac0ce6521b7f
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 15:30:39 +0900

    [update]ユーザの編集成功時のテストコードを実装

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,4 +20,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template "users/edit"
     assert_select "div.alert", "The form contains 3 errors"
   end
+
+  test "succsessful user update" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    name  = "Update"
+    email = "Update@exsample.com" 
+
+    patch user_path(@user), params:{
+      user{
+        name:                  name,
+        email:                 email,
+        password:              "",
+        password_confirmation: ""
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
