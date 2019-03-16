64501e9d163424ba74d265b9eff856b7090534d0
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 10:52:05 +0900

    [add]loguout機能とテストコードの実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,12 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+
+  def destroy
+    log_out
+    redirect_to root_url
   #Private Method
   private
-  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
