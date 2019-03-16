8dd28ec13baf68545297df188c405a8cdbb2de84
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 14:02:31 +0900

    [fix]

--- test/helpers/session_helper_test.rb
+++ test/helpers/session_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SessionsHelperTest < ActionView::TestCase
 
     def setup
-        @user = user(:michael)
+        @user = users(:michael)
         remember(@user)
     end
 
