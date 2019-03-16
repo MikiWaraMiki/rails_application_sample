de36a247082f45472872c5306c0b24763bacbad4
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 11:09:20 +0900

    [add]token生成メソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {maximum:50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,4 +14,11 @@ class User < ApplicationRecord
         #ハッシュ値でパスワードを登録する
         BCrypt::Password.create(string, cost: cost)
     end
+
+    def User.new_token
+        SecurePassword.urlsafe_base64
+
+    def remember
+        self.remember = User.new_token
+        update_attribute(:remember_token, User.digest(remember_token))
 end
