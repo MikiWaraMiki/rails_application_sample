f744a68da6027c66775cdb9e39ffe6550de4b614
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 16:13:40 +0900

    [fix]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,6 @@ class UsersController < ApplicationController
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root) unless @user == currect_user
+    redirect_to(root) unless @user == current_user
   end
 end
