426ceeb07c4f1462a789ca9006e1bb48eca18534
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 16:39:57 +0900

    [add]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,4 +37,12 @@ module SessionsHelper
         @current_user = nil
     end
 
+    def redirecr_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
 end
