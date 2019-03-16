27fa2a514d9967470a9db6e203e864b458318f7d
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 15:13:19 +0900

    [update]編集追加時のテストコードを実装

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessfull user update" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+
+    patch user_path(@user), params:{
+      user:{
+        name:"",
+        email:"valid@exsample.com",
+        password:"foo",
+        password_confirmation: "foo",
+      }
+    }
+    assert_template "users/edit"
+  end
 end
