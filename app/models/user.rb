464be3cb72f825608df3782fd1eb6f8486d37353
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 10:08:50 +0900

    [fix]Userモデルのバグフィックス

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: {minimum:6}
 
-    def User.digest(strig)
+    def User.digest(string)
         #ハッシュ値を返す
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
