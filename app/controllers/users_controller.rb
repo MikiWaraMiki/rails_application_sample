889457aa66e547f479d25ba86a1600424ddf7e2b
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 16:39:51 +0900

    [add]フレンドリーフォワーディング機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample app"
-      redirect_to @user
+      redirect_back_or @user
     else
       render "new"
     end
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please Login"
       redirect_to login_url
     end
