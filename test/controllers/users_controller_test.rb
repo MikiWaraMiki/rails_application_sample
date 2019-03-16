ad33d11cce06003af57f1c6e026f857c6b33c250
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-16 19:53:11 +0900

    [fix]

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,10 +10,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should redirect index when not logged in" do
-    get users_path
-    assert_redirected_to login_url
-  end
+ 
 
   test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
