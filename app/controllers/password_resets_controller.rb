3bc9e39a45ddb615d9c7e10f0874ec66b988f186
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 16:53:31 +0900

    [add]パスワード再設定時のバリデーション機能を実装

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only:[:edit, :update]
   before_action :valid_user, only:[:edit, :update]
+  before_action :check_explation, only:[:edit, :update]
   def new
   end
 
@@ -17,7 +18,23 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
   def get_user
     @user = User.find_by(email:params[:email])
   end
@@ -27,4 +44,12 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     end
   end
+
+  def check_explation
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired"
+      redirect_to root_url
+    end
+  end
+
 end
