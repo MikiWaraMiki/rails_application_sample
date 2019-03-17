ada58609f6fa96afb547ecc0a5876bf0bc12f897
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 16:25:02 +0900

    [fix]

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -20,8 +20,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@exsample.com"], mail.from
-    assert_match user.name, mail.body.encoded
-    assert_match user.activation_token,  mail.body.encoded
+    assert_match user.reset_token,  mail.body.encoded
     assert_mathc CGI.escape(user.email), mail.body.encoded
   end
 end
