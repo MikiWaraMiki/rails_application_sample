f99d80249b5f3dcc086c5eeda10a687e0c3977bc
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 17:14:16 +0900

    [fix]

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -21,7 +21,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
       password_reset:{email:@user.email}
     }
     assert_not_equal @user.reset_digest, @user.reload.reset_digest
-    assert_equal 1, ActionMailer::Base.deliveries.suze
+    assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirected_to root_url
     #パスワード再設定のフォーム検証
