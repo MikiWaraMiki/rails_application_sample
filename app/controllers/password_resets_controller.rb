d2cc94f7fcca008892fe9cea3cd58611b3bfc06c
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 15:46:25 +0900

    [update]パスワードリセットのメール送信機能を追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,15 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(email:params[:password_rest][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:success] = "Email sent with password reset instruction"
+      redirect_to root_url
+    else
+      flash[:danger] = "Email address not Found"
+      redirect_to root_url
+    end
   end
 end
