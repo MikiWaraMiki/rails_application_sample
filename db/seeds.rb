ecbdce4ad038b24f540a0f1505164b08987c55cc
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 11:09:09 +0900

    [update]有効化フラグをつけたマイグレーションデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@ User.create!(name: "Exsample",
     email: "exsample@railstutorial.org",
     password: "password",
     password_confirmation: "password",
-    admin: true
+    admin: true,
+    activated: true,
+    activated_at: Time.zone.now
     )
 99.times do |n|
     name  = Faker::Name.name
@@ -18,6 +20,8 @@ User.create!(name: "Exsample",
     User.create!(name: name,
         email: email,
         password: password,
-        password_confirmation: password
+        password_confirmation: password,
+        activated: true,
+        activated_at: Time.zone.now,
     )
 end
