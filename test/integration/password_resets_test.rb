1122ed23ac2d44cef9adf74b4491a8a5972c9c3b
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-17 17:11:02 +0900

    [fix]

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PasswordResetsTest < ActionDispatch::IntegrationTest
   def setup
     ActionMailer::Base.deliveries.clear
-    @user = users(:micahel)
+    @user = users(:michael)
   end
 
   test "password reset" do
