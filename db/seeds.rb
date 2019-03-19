a7d5a6f583347f3b55f65534f11c9c3082027944
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-19 23:05:34 +0900

    [fix]

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ User.create!(name: "Exsample",
 end
 users = User.order(:created_at).take(6)
 50.times do 
-    content = Facker::Lorem.sentence(5)
+    content = Faker::Lorem.sentence(5)
     users.each {|user| user.microposts.create!(content: content)}
 end
 
