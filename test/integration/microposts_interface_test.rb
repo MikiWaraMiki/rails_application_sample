6c15c65ad431f0732a8a5c7a0215503efbdd3bf6
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-21 14:09:35 +0900

    [add]記事投稿数テストを追加

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -30,4 +30,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       delete micropost_path(first_micropost)
     end
   end
+
+  test "micropost count sidevar" do
+    log_in_as(@user)
+    get root_path
+    micrposts_count = user.microposts.count 
+    assert_match "#{micrposts_count} microposts" , response.body
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_math "0 microposts", response.body
+    other_user.microposts.create!(content:"A micropost")
+    get root_path
+    assert_match "1 microposts", reseponse.body
+  end
 end
