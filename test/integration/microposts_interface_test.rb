da760c15a29913adc44d2f23c87fb191253ac640
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-21 14:10:04 +0900

    [fix]

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -34,7 +34,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost count sidevar" do
     log_in_as(@user)
     get root_path
-    micrposts_count = user.microposts.count 
+    micrposts_count = @user.microposts.count 
     assert_match "#{micrposts_count} microposts" , response.body
     other_user = users(:malory)
     log_in_as(other_user)
