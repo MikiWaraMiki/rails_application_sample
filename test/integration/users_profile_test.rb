2fbb7fbb289a3a76ee6ed1b31d920fa5238cf88b
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-03-21 12:01:50 +0900

    [fix]

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -13,7 +13,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text:@user.name
     assert_select 'h1>img.gravatar'
-    assert_match @user.microposts.count.to_s, response,body
+    assert_match @user.microposts.count.to_s, response.body
     @user.microposts.paginate(page:1).each do |post| 
       assert_match post.content, response.body
     end
